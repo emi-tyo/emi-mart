5ba7c415e4bf20a0499006e68483bbefa7109cfd
Emi Sekikawa <emiitabashi7@gmail.com>
2025-04-24 13:59:24 +1000

    Fix Add to Cart button issue in search.php

--- pages/search.php
+++ pages/search.php
@@ -11,10 +11,15 @@ $result = $conn->query($query);
 echo "<div class='grid'>";
 while ($row = $result->fetch_assoc()) {
     $image = !empty($row['image']) ? '../assets/images/' . $row['image'] : '../assets/images/default.png';
+    
     echo "<div class='product'>
-            <img src='$image' alt='{$row['name']}' height='160'>
-            <p>{$row['name']}</p>
+            <img src='$image' alt='{$row['name']}' width='160'>
+            <p><strong>{$row['name']}</strong></p>
             <p>\${$row['price']}</p>
+            <form method='POST' action='../functions/cart_functions.php'>
+                <input type='hidden' name='product_id' value='{$row['id']}'>
+                <button type='submit' name='add_to_cart'>Add to Cart</button>
+            </form>
           </div>";
 }
 echo "</div>";
